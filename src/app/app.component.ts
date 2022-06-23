e392908c46ce67b58c63f8239ff56f072c38e2fa
fabioemoutinho <fabioemoutinho@gmail.com>
2022-06-23 10:08:52 -0500

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { tap } from 'rxjs';
+
+enum LANGUAGES {
+  java = 'javascript',
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent implements OnInit {
+  readonly form = this.fb.group({
+    input: [''],
+    output: [''],
+  });
+  private readonly languages = LANGUAGES;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.form.controls.input.valueChanges
+      .pipe(
+        tap((input) => {
+          this.form.controls.output.setValue(this.clearHtml(input!));
+        })
+      )
+      .subscribe();
+  }
+
+  clearHtml(htmlString: string): string {
+    return htmlString
+      .replace(/<\/?span[^>]*>/g, '') // remove span, but keep content
+      .replace(
+        /<\s*pre.*?data-syntaxhighlighter-params=\"brush: (.*?); [^"]*\".*?>/g,
+        (match, p1: keyof typeof LANGUAGES) => {
+          const language: string = this.languages[p1] ?? 'javascript';
+          return `<pre class="language-${language}">`;
+        }
+      ) // replace attributes inside <pre>
+      .replace(/(<pre.*?>)([^]*?)(<\/pre>)/g, '$1<code>$2</code>$3') // wrap content inside <pre> with <code>
+      .replace(/<\/?div[^>]*>/g, ''); // remove div, but keep content
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ClipboardModule } from '@angular/cdk/clipboard';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ClipboardModule,
+    ReactiveFormsModule,
+    MatInputModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
